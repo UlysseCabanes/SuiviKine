9c6aaef51ab1937867bf88c1f7312acfc9c0b1de
ulyss <ulyss@LAPTOP-OD9FT2I8>
2020-05-14 11:13:58 +0200

    Initialisation

---
+++ src/main/java/jardinsdeflore/suivikine/SuiviKineApplication.java
@@ -0,0 +1,13 @@
+package jardinsdeflore.suivikine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuiviKineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuiviKineApplication.class, args);
+    }
+
+}
