b3c92b227f8c5a471aea1060395f81b60daa7f2b
ulyss <ulyss@LAPTOP-OD9FT2I8>
2020-05-26 17:32:36 +0200

    Connexion à la BDD

---
+++ src/test/java/jardinsdeflore/suivikine/SuiviKineApplicationTests.java
@@ -0,0 +1,13 @@
+package jardinsdeflore.suivikine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuiviKineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
