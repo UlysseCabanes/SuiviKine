3d8cab2f372e0f9506b4c7153c4ca85850940508
ulyss <ulyss@LAPTOP-OD9FT2I8>
2020-07-03 12:45:19 +0200

    Configuration MySQL

--- src/main/java/jardinsdeflore/suivikine/entity/EquipeKine.java
+++ src/main/java/jardinsdeflore/suivikine/entity/EquipeKine.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
@@ -13,7 +14,7 @@ import javax.persistence.UniqueConstraint;
 public class EquipeKine implements Serializable{
     
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name = "id_equipe_kine")
     private int idEquipeKine;
     
--- src/main/java/jardinsdeflore/suivikine/entity/Medecin.java
+++ src/main/java/jardinsdeflore/suivikine/entity/Medecin.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,7 +13,7 @@ import javax.persistence.Table;
 public class Medecin implements Serializable{
     
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name = "id_medecin")
     private int idMedecin;
     
